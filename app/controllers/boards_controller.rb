7381f6dbf0cf660cfde7560c6d4c8990dfe691bf
yuzu5 <aoki08160816@gmail.com>
2019-03-18 23:28:30 +0900

    掲示板新規作成機能追加

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,19 @@
 class BoardsController < ApplicationController
+
   def index
+  end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    Board.create(board_params)
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:name, :title, :body)
   end
 end
