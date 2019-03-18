d86cbb331045f9945e06bb842e1d172e02a01966
yuzu5 <aoki08160816@gmail.com>
2019-03-19 02:39:51 +0900

    コントローラーをリファクタリング

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,5 @@
 class BoardsController < ApplicationController
+  before_action :set_target_board, only: %i[show edit update destroy]
 
   def index
     @boards = Board.all
@@ -14,23 +15,20 @@ class BoardsController < ApplicationController
   end
 
   def show
-    @board = Board.find(params[:id])
   end
 
   def edit
-    @board = Board.find(params[:id])
   end
 
   def update
-    board = Board.find(params[:id])
-    board.update(board_params)
+    @board.update(board_params)
 
-    redirect_to board
+    redirect_to @board
   end
 
   def destroy
-    board = Board.find(params[:id])
-    board.destroy
+
+    @board.destroy
 
     redirect_to boards_path
   end
@@ -39,5 +37,9 @@ class BoardsController < ApplicationController
 
   def board_params
     params.require(:board).permit(:name, :title, :body)
+  end
+
+  def set_target_board
+    @board = Board.find(params[:id])
   end
 end
